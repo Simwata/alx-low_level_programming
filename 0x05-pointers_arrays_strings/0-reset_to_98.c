2489022ff02aa9357077f6dd730116c75c18b787
Simwata <simwatasamuel@gmail.com>
2022-06-14 22:51:15 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - takes pointer to an int parameterand updates the value it points to 98
+ *
+ *@n: int parameter
+ *
+ * Return:Nothing
+ */
+
+void reset_to_98(int *n)
+{
+  *n=98;
+}
